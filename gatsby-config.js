7831d046dd90251244c21d4c10459538d0657bfd
Khushnood Asif <khushnood99@gmail.com>
2021-09-03 11:43:36 +0100

    update site folder

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 const config = require('./src/config');
 
 module.exports = {
-  pathPrefix: '/portfolio-website',
+  pathPrefix: '/khushnoodasif.github.io',
   siteMetadata: {
     title: config.siteTitle,
     siteUrl: config.siteUrl,
