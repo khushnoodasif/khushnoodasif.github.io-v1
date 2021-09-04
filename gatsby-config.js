9f9ee9ce3f84e4d1ba5fd4a9937b5862176bb128
Khushnood Asif <khushnood99@gmail.com>
2021-09-04 18:37:07 +0100

    update pathprefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,6 @@
 const config = require('./src/config');
 
 module.exports = {
-  pathPrefix: '/khushnoodasif.github.io',
   siteMetadata: {
     title: config.siteTitle,
     siteUrl: config.siteUrl,
